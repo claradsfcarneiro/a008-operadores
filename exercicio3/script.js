1505c8de47781afd9f23f021ac1d176609ee31b7
Clara <claradsfcarneiro@gmail.com>
2022-08-30 14:38:36 -0300

    Realizar exercício 3

---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+const operacaoA = 5 > 20 && 5 < 2;
+const operacaoB = 5 === 5 || 5 === "5";
+const operacaoC = !(20 > 50);
+const operacaoD = !(20 > 50 || 50 > 70);
+
+console.log(operacaoA);
+console.log(operacaoB);
+console.log(operacaoC);
+console.log(operacaoD);
