08ca876af8a85171946ad81b2d9e42dea5640d21
Clara <claradsfcarneiro@gmail.com>
2022-08-31 16:24:40 -0300

    Declarar variáveis do exercício 2 como const

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,5 +1,5 @@
-primeiroNumero = Number(prompt("Digite um número"));
-segundoNumero = Number(prompt("Digite outro número"));
+const primeiroNumero = Number(prompt("Digite um número"));
+const segundoNumero = Number(prompt("Digite outro número"));
 
 console.log(`O primeiro numero é maior que segundo? ${primeiroNumero > segundoNumero}`);
 console.log(`O primeiro numero é igual ao segundo? ${primeiroNumero === segundoNumero}`);
